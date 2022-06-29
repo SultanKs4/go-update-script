329c10e68f85c7c7f7096973eaca2ce3805de5c0
Sultan Achmad <sultangendut@gmail.com>
2022-06-29 07:52:54 +0700

    fix: check OS and architecture automatic chore: update to 1.18.3 ver

--- update.sh
+++ update.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
 
 rm -rf /usr/local/go*
-rm -rf /usr/local/go
 
-VERSION=1.18
-OS=linux
-ARCH=amd64
+VERSION=1.18.3
+OS=$(uname | awk '{print tolower($0)}')
+ARCH=$(uname -m | awk '{print tolower($0)}')
+
+case $ARCH in
+    x86_64)
+        ARCH="amd64"
+        ;;
+    x86)
+        ARCH="386"
+        ;;
+    aarch64)
+        ARCH="arm64"
+        ;;
+esac
 
 cd $HOME
 rm go$VERSION.$OS-$ARCH.tar.gz
